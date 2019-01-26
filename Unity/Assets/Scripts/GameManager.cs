27f2f08a3af13a21c427b57b8871e8295646d95b
Esefen <romain.soual.pro@gmail.com>
2019-01-26 16:14:19 +0100

    Basic prototype

---
+++ Unity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AppState {Menu, Question, Answer, Transition};
+
+public struct Question
+{
+    public AudioClip intro;
+    public AudioClip[] answers;
+}
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public static AppState gameState = AppState.Question;
+    public static AudioSource speaker;
+    public Question test;
+
+    bool fadeOut = false;
+
+    void Awake()
+    {
+        Instance = this;
+        speaker = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayAnswer(AudioClip answer)
+    {
+        if (!speaker.isPlaying || speaker.clip != answer)
+        {
+            StopAllCoroutines();
+            //speaker.volume = 1;
+            speaker.clip = answer;
+            speaker.Play();
+        }
+    }
+
+    public void UpdateAnswerSound(float newVolume)
+    {
+        fadeOut = false;
+        speaker.volume = newVolume;
+    }
+
+    public void FadeOutSpeaker()
+    {
+        if (!fadeOut)
+        {
+            fadeOut = true;
+            iTween.AudioTo(gameObject, 0, 1, 0.7f);
+        }
+        //iTween.AudioTo(gameObject, iTween.Hash(""));
+    }
+
+    public IEnumerator FadeOut()
+    {
+        while (speaker.volume >= 0)
+        {
+            yield return new WaitForFixedUpdate();
+            speaker.volume -= 0.1f * Time.deltaTime;
+        }
+    }
+
+    public void DiminishSound()
+    {
+        speaker.volume -= 0.5f * Time.deltaTime;
+    }
+}
---
+++ Unity/Assets/Scripts/PlayerSoundscape.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSoundscape : MonoBehaviour
+{
+    const float threshold = 0.65f;
+
+    float xMin, yMin;
+    Vector2 center;
+
+    Question currentQuestion;
+    public AudioClip testHautGauche;
+    public AudioClip testHautDroite;
+    public AudioClip testBasGauche;
+    public AudioClip testBasDroite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager.gameState = AppState.Answer;
+
+        xMin = 1 - threshold;
+        yMin = 1 - threshold;
+        center = new Vector2(Screen.width / 2, Screen.height / 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Only active in answer mode
+        if (GameManager.gameState == AppState.Answer)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Vector2 pos = new Vector2(Input.mousePosition.x / Screen.width, Input.mousePosition.y / Screen.height);
+                UpdateInteractiveSoundscape(pos);
+                UpdateSoundVolume(pos);
+            }
+            // Return soundscape to default
+            else
+            {
+                GameManager.Instance.FadeOutSpeaker();
+                //StartCoroutine(GameManager.Instance.FadeOut());
+                //GameManager.speaker.Stop();
+            }
+        }
+    }
+
+    // Modify soundscape
+    void UpdateInteractiveSoundscape(Vector2 pos)
+    {
+        //Debug.Log("Mouse Position: " + Input.mousePosition.x + ";" + Input.mousePosition.y);
+        //Debug.Log("Mouse Position: " + pos.x + ";" + pos.y);
+        // left
+        if (pos.x < xMin)
+        {
+            // bottom
+            if (pos.y < yMin)
+            {
+                GameManager.Instance.PlayAnswer(testBasGauche);
+            }
+            // top
+            else if (pos.y > threshold)
+            {
+                GameManager.Instance.PlayAnswer(testHautGauche);
+            }
+        }
+        // right
+        else if (pos.x > threshold)
+        {
+            // bottom
+            if (pos.y < yMin)
+            {
+                GameManager.Instance.PlayAnswer(testBasDroite);
+            }
+            // top
+            else if (pos.y > threshold)
+            {
+                GameManager.Instance.PlayAnswer(testHautDroite);
+            }
+        }
+    }
+
+    void UpdateSoundVolume(Vector2 pos)
+    {
+        float d = Vector3.Distance(center, Input.mousePosition) / ((float)Screen.width /2f);
+        Debug.Log("Mouse distance from center: " + d);
+        GameManager.Instance.UpdateAnswerSound(Mathf.Min(d, 1));
+    }
+
+}
